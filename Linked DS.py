07493a6fada5dbb989046eee64bcc05ca2dce339
Piyush <piyushjain1857@gmail.com>
2026-01-27 21:37:28 +0530

    added loop

--- Linked DS.py
+++ Linked DS.py
@@ -4,3 +4,5 @@ arrName = arr.array('i', [1, 2, 3, 4, 5])
 print("Array Name:", arrName)
 print("Type of arrName:", type(arrName))
 print("Array elements:")
+for element in arrName:
+    print(element)  
