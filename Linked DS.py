a13ccc2b958ae9d2f20f4fa5e6b9383dbdd1b4cf
Piyush <piyushjain1857@gmail.com>
2026-01-27 22:15:59 +0530

    test

--- Linked DS.py
+++ Linked DS.py
@@ -5,4 +5,22 @@ print("Array Name:", arrName)
 print("Type of arrName:", type(arrName))
 print("Array elements:")
 for element in arrName:
-    print(element)  
+    print(element)
+    
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not Prime")
+else:
+    is_prime = True
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            is_prime = False
+            break
+        i += 1
+
+    if is_prime:
+        print("Prime Number")
+    else:
+        print("Not Prime")
