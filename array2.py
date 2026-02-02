25b99d29eb50e75b59c2333cf1045f9cd62e9f0b
Piyush <piyushjain1857@gmail.com>
2026-02-02 20:50:39 +0530

    added more text

--- array2.py
+++ array2.py
@@ -1,24 +1,26 @@
-import math
-def missing_number(num):
-    n = len(num)
-    expected_sum = n * (n + 1) // 2
-    actual_sum = sum(num)
-    return expected_sum - actual_sum
-arr = [9, 6, 4, 2, 3, 5, 7, 0, 1]
-print(f" Missing number: {missing_number(arr)}")
+# import math
+# def missing_number(num):
+#     n = len(num)
+#     expected_sum = n * (n + 1) // 2
+#     actual_sum = sum(num)
+#     return expected_sum - actual_sum
+# arr = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+# print(f" Missing number: {missing_number(arr)}")
 
-max_num = arr[0]
-for num in arr:
-    if num > max_num:
-        max_num = num
-print(f"Maximum number: {max_num}")
+# max_num = arr[0]
+# for num in arr:
+#     if num > max_num:
+#         max_num = num
+# print(f"Maximum number: {max_num}")
 
-second_max = -math.inf
-for num in arr:
-    if num > second_max and num < max_num:
-        second_max = num
-print(f"Second maximum number: {second_max}")
+# second_max = -math.inf
+# for num in arr:
+#     if num > second_max and num < max_num:
+#         second_max = num
+# print(f"Second maximum number: {second_max}")
 
 
 
-print("hello world")
+# print("hello world")
+
+
