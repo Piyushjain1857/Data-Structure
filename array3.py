4ecc253ac21a46ef8092da93ab13b9b0feaa48d5
Piyush <piyushjain1857@gmail.com>
2026-02-03 21:16:46 +0530

    commented the code

--- array3.py
+++ array3.py
@@ -1,16 +1,16 @@
 import array as arr
 
-arr1 = [1, 2, 3, 4, 5, 6, 7]
-k=3
+# arr1 = [1, 2, 3, 4, 5, 6, 7]
+# k=3
 
-def Rotate(arr,k):
-    last = arr[-1]
-    while k>0:
-        for i in range(len(arr)-1, 0, -1):
-            arr[i] = arr[i-1]
-        arr[0] = last
-        return arr
-        k -= 1
-    return arr
-print(Rotate(arr1,k))
+# def Rotate(arr,k):
+#     last = arr[-1]
+#     while k>0:
+#         for i in range(len(arr)-1, 0, -1):
+#             arr[i] = arr[i-1]
+#         arr[0] = last
+#         return arr
+#         k -= 1
+#     return arr
+# print(Rotate(arr1,k))
 
