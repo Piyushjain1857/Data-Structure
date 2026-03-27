5b4648b0eb9e868786e28c9dfbdbba3abc7dfe79
Piyush <piyushjain1857@gmail.com>
2026-03-27 14:01:04 +0530

    added double linked list

--- linked_list/Double_linkedlist.py
+++ linked_list/Double_linkedlist.py
@@ -1,97 +1,148 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
 
 
-# # Doubly Linked List ADT Implementation in Python
+class DoublyLinkedListADT:
+    def __init__(self):
+        self.head = None
 
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.prev = None
-#         self.next = None
+    # Check if list is empty
+    def is_empty(self):
+        return self.head is None
 
+    # Insert at beginning
+    def insertAtFirstPos(self, data):
+        new_node = Node(data)
 
-# class DoublyLinkedList:
-#     def __init__(self):
-#         self.head = None
+        if self.head is not None:
+            self.head.prev = new_node
+            new_node.next = self.head
 
-#     # Insert at beginning
-#     def insert_at_beginning(self, data):
-#         new_node = Node(data)
-#         if self.head is not None:
-#             self.head.prev = new_node
-#             new_node.next = self.head
-#         self.head = new_node
+        self.head = new_node
 
-#     # Insert at end
-#     def insert_at_end(self, data):
-#         new_node = Node(data)
-#         if self.head is None:
-#             self.head = new_node
-#             return
-        
-#         temp = self.head
-#         while temp.next:
-#             temp = temp.next
-        
-#         temp.next = new_node
-#         new_node.prev = temp
+    # Insert at end
+    def insertAtLastPos(self, data):
+        new_node = Node(data)
 
-#     # Delete a node
-#     def delete(self, key):
-#         temp = self.head
+        if self.is_empty():
+            self.head = new_node
+            return
 
-#         while temp:
-#             if temp.data == key:
-#                 # If node to be deleted is head
-#                 if temp.prev is None:
-#                     self.head = temp.next
-#                     if self.head:
-#                         self.head.prev = None
-#                 else:
-#                     temp.prev.next = temp.next
-#                     if temp.next:
-#                         temp.next.prev = temp.prev
-#                 return
-#             temp = temp.next
-#         print("Value not found")
+        temp = self.head
+        while temp.next:
+            temp = temp.next
 
-#     # Display forward
-#     def display_forward(self):
-#         temp = self.head
-#         while temp:
-#             print(temp.data, end=" <-> ")
-#             temp = temp.next
-#         print("None")
+        temp.next = new_node
+        new_node.prev = temp
 
-#     # Display backward
-#     def display_backward(self):
-#         temp = self.head
-#         if temp is None:
-#             return
-        
-#         while temp.next:
-#             temp = temp.next
-        
-#         while temp:
-#             print(temp.data, end=" <-> ")
-#             temp = temp.prev
-#         print("None")
+    def insertAtPos(self, data, pos):
+        new_node = Node(data)
 
+        if pos == 0:
+            self.insertAtFirstPos(data)
+            return
 
-# # Example usage
-# if __name__ == "__main__":
-#     dll = DoublyLinkedList()
+        temp = self.head
+        count = 0
 
-#     dll.insert_at_beginning(10)
-#     dll.insert_at_beginning(5)
-#     dll.insert_at_end(20)
-#     dll.insert_at_end(25)
+        while temp and count < pos - 1:
+            temp = temp.next
+            count += 1
 
-#     print("Forward:")
-#     dll.display_forward()
+        if temp is None:
+            print("Position out of range")
+            return
 
-#     print("Backward:")
-#     dll.display_backward()
+        new_node.next = temp.next
+        new_node.prev = temp
 
-#     dll.delete(20)
-#     print("After deletion:")
-#     dll.display_forward()
+        if temp.next:
+            temp.next.prev = new_node
+
+        temp.next = new_node
+
+    # Delete from front
+    def deleteAtFirstPos(self):
+        if self.is_empty():
+            print("List is empty")
+            return
+
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+
+    # Delete from end
+    def deleteAtLastPos(self):
+        if self.is_empty():
+            print("List is empty")
+            return
+
+        temp = self.head
+        if temp.next is None:
+            self.head = None
+            return
+
+        while temp.next:
+            temp = temp.next
+
+        temp.prev.next = None
+
+    def deleteAtPos(self, pos):
+        if self.is_empty():
+            print("List is empty")
+            return
+
+        if pos == 0:
+            self.deleteAtFirstPos()
+            return
+
+        temp = self.head
+        count = 0
+
+        while temp and count < pos:
+            temp = temp.next
+            count += 1
+
+        if temp is None:
+            print("Position out of range")
+            return
+
+        if temp.next:
+            temp.next.prev = temp.prev
+
+        if temp.prev:
+            temp.prev.next = temp.next
+
+    # Search element
+    def search(self, key):
+        temp = self.head
+        while temp:
+            if temp.data == key:
+                return True
+            temp = temp.next
+        return False
+
+    # Display forward
+    def display_forward(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" <-> ")
+            temp = temp.next
+        print("None")
+
+    # Display backward
+    def display_backward(self):
+        temp = self.head
+        if temp is None:
+            return
+
+        while temp.next:
+            temp = temp.next
+
+        while temp:
+            print(temp.data, end=" <-> ")
+            temp = temp.prev
+        print("None")
