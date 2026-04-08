10c2de5ed253e2b8d11fefa8b9b413435e34441b
Piyush <piyushjain1857@gmail.com>
2026-04-08 11:09:07 +0530

    code refactored

--- test.py
+++ test.py
@@ -86,17 +86,17 @@
 #     return arr[-k:] + arr[:-k]
 
 
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(0,n-1):
-        min_index = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(0,n-1):
+#         min_index = i
+#         for j in range(i + 1, n):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+#     return arr
 
 
-arr = [64, 25, 12, 22, 11]
-sorted_arr = selection_sort(arr)
-print("Sorted array:", sorted_arr)
+# arr = [64, 25, 12, 22, 11]
+# sorted_arr = selection_sort(arr)
+# print("Sorted array:", sorted_arr)
