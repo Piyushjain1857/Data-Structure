668e5f706114494e0b3ebe0d570599d97340aa7f
Piyush <piyushjain1857@gmail.com>
2026-04-14 11:22:16 +0530

    added sorting

--- sorting/insertionSort.py
+++ sorting/insertionSort.py
@@ -12,24 +12,3 @@ def insertion_sort(arr):
 arr = [7, -8, 2, 3, 0, 5, -18]
 sorted_arr = insertion_sort(arr)
 print("Sorted array:", sorted_arr)
-
-
-
-# def insertion_sort(arr):
-#     n = len(arr)
-#     for i in range(1, n):
-#         min = arr[i]
-#         j = i - 1
-
-#         while j >= 0 and arr[j] > min:
-#             arr[j + 1] = arr[j]
-#             j -= 1
-
-#         arr[j + 1] = min
-
-#     return arr
-
-# # Example usage
-# arr = [64, 25, 12, 22, 11]
-# sorted_arr = insertion_sort(arr)
-# print("Sorted array:", sorted_arr)
---
+++ sorting/quickSort.py
@@ -0,0 +1,20 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for x in arr[1:]:
+        if x <= pivot:
+            left.append(x)
+        else:
+            right.append(x)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# Example usage
+arr = [64, 25, 12, 22, 11]
+sorted_arr = quick_sort(arr)
+print("Sorted array:", sorted_arr)
