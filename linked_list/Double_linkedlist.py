51642d316d50ada94690f52c1e28eec282cdaae8
Piyush Jain <piyushjain1857@gmail.com>
2026-08-23 15:11:01 +0530

    fixed bugs again

--- linked_list/Double_linkedlist.py
+++ linked_list/Double_linkedlist.py
@@ -3,9 +3,8 @@ class Node:
         self.prev = None
         self.data = myData
         self.next = None
+       
 
-        
-        
 class DoublyLinkedListADT:
     def __init__(self):
         self.head = None
